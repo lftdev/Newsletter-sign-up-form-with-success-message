354f71cdfd1aeae6ab8b6eb6f8cd0034d4c0d01a
hydrae-soft <torres.lf21@gmail.com>
2023-06-13 16:27:50 -0300

    Prepare for deploy 2.

--- vite.cofing.ts
+++ vite.cofing.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Newsletter-sign-up-form-with-success-message/'
+  base: "/Newsletter-sign-up-form-with-success-message/"
 })
